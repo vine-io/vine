d8512f13b61ec3c06ea3457b198d734f10706d68
xingyys <598223084@qq.com>
2021-01-19 17:25:52 +0800

    modify

--- proto/registry/registry.pb.go
+++ proto/registry/registry.pb.go
@@ -5,10 +5,11 @@ package registry
 
 import (
 	fmt "fmt"
-	proto "github.com/gogo/protobuf/proto"
 	io "io"
 	math "math"
 	math_bits "math/bits"
+
+	proto "github.com/gogo/protobuf/proto"
 )
 
 // Reference imports to suppress errors if they are not otherwise used.
@@ -1361,8 +1362,8 @@ func (m *PathRequestBody) GetContent() *PathRequestBodyContent {
 }
 
 type PathRequestBodyContent struct {
-	ApplicationJson *ApplicationContent `protobuf:"bytes,1,opt,name=applicationJson,proto3" json:"applicationJson,omitempty"`
-	ApplicationXml  *ApplicationContent `protobuf:"bytes,2,opt,name=applicationXml,proto3" json:"applicationXml,omitempty"`
+	ApplicationJson *ApplicationContent `protobuf:"bytes,1,opt,name=applicationJson,proto3" json:"application/json,omitempty"`
+	ApplicationXml  *ApplicationContent `protobuf:"bytes,2,opt,name=applicationXml,proto3" json:"application/xml,omitempty"`
 }
 
 func (m *PathRequestBodyContent) Reset()         { *m = PathRequestBodyContent{} }
@@ -1880,7 +1881,7 @@ type Schema struct {
 	ReadOnly         bool              `protobuf:"varint,7,opt,name=readOnly,proto3" json:"readOnly,omitempty"`
 	WriteOnly        bool              `protobuf:"varint,8,opt,name=writeOnly,proto3" json:"writeOnly,omitempty"`
 	Required         bool              `protobuf:"varint,9,opt,name=required,proto3" json:"required,omitempty"`
-	Ref              string            `protobuf:"bytes,10,opt,name=ref,proto3" json:"ref,omitempty"`
+	Ref              string            `protobuf:"bytes,10,opt,name=ref,proto3" json:"$ref,omitempty"`
 	Default          string            `protobuf:"bytes,11,opt,name=default,proto3" json:"default,omitempty"`
 	MinLength        int32             `protobuf:"varint,20,opt,name=minLength,proto3" json:"minLength,omitempty"`
 	MaxLength        int32             `protobuf:"varint,21,opt,name=maxLength,proto3" json:"maxLength,omitempty"`
