e0288d9c4026819f63adaa6351bd48ae641f6cba
lack <598223084@qq.com>
2021-02-20 08:54:52 +0800

    modify

--- service/broker/options.go
+++ service/broker/options.go
@@ -49,6 +49,7 @@ type SubscribeOptions struct {
 	// AutoAck defaults to true. When a handler returns
 	// with a nil error the message is acked.
 	AutoAck bool
+
 	// Subscribers with the same queue name
 	// will create a shared subscription where each
 	// receives a subset of messages.
