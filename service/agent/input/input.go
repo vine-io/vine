eb9f68390bc30819672fe9948223ab2fc41b5653
lack <598223084@qq.com>
2021-04-09 17:26:56 +0800

    modify LICENSE

--- service/agent/input/input.go
+++ service/agent/input/input.go
@@ -1,14 +1,24 @@
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// MIT License
 //
-//     http://www.apache.org/licenses/LICENSE-2.0
+// Copyright (c) 2020 Lack
 //
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
 
 // Package input is an interface for bot inputs
 package input
@@ -42,9 +52,9 @@ type Event struct {
 // provide a way to communicate with the bot.
 // Slack, HipChat, XMPP, etc.
 type Input interface {
-	// Provide cli flags
+	// Flags Provide cli flags
 	Flags() []cli.Flag
-	// Initialise input using cli context
+	// Init Initialise input using cli context
 	Init(*cli.Context) error
 	// Stream events from the input
 	Stream() (Conn, error)
@@ -52,7 +62,7 @@ type Input interface {
 	Start() error
 	// Stop the input
 	Stop() error
-	// name of the input
+	// String name of the input
 	String() string
 }
 
