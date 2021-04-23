a78a91cadce94df273eeb4e11b995b63019b5eb6
lack <598223084@qq.com>
2021-04-23 12:57:06 +0800

    modify

--- cmd/vine/app/cli/new/template/module.go
+++ cmd/vine/app/cli/new/template/module.go
@@ -5,6 +5,10 @@ var (
 
 go 1.15
 
+require (
+	github.com/lack-io/vine v0.14.0
+)
+
 // This can be removed once etcd becomes go gettable, version 3.4 and 3.5 is not,
 // see https://github.com/etcd-io/etcd/issues/11154 and https://github.com/etcd-io/etcd/issues/11931.
 replace google.golang.org/grpc => google.golang.org/grpc v1.26.0
