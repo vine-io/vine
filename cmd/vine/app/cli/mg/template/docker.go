d111fce28c5682640c8d709ce2d7585d2d3ea4f5
lack <598223084@qq.com>
2021-09-08 16:41:51 +0800

    template docker

--- cmd/vine/app/cli/mg/template/docker.go
+++ cmd/vine/app/cli/mg/template/docker.go
@@ -1,9 +1,12 @@
 package template
 
 var (
-	DockerSRV = `FROM alpine
+	DockerSRV = `FROM debian:stable-slim
 ADD {{.Name}} /{{.Name}}
-ENTRYPOINT [ "/{{.Name}}" ]
+
+EXPOSE 11500
+
+ENTRYPOINT [ "/{{.Name}}", "--server-address=0.0.0.0:11500" ]
 `
 
 	DockerWEB = `FROM alpine
