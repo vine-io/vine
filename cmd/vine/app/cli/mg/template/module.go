5251676fd59281e1f78a90bf5d5cb9448a9c29dd
lack <598223084@qq.com>
2021-12-21 15:35:48 +0800

    template

--- cmd/vine/app/cli/mg/template/module.go
+++ cmd/vine/app/cli/mg/template/module.go
@@ -7,6 +7,7 @@ go {{.GoVersion}}
 
 require (
 	github.com/vine-io/vine {{.VineVersion}}
+	github.com/vine-io/apimachinery {{.VineVersion}}
 )
 
 replace google.golang.org/grpc => google.golang.org/grpc v1.38.0
