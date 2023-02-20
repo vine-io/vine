0ac577ca6e454f43516d9436fa01afdb9a941a3f
lack-io <598223084@qq.com>
2023-02-20 14:59:11 +0800

    v1.5.0

--- cmd/vine/version/version.go
+++ cmd/vine/version/version.go
@@ -7,9 +7,9 @@ import (
 )
 
 var (
-	GitCommit = "8e725320"
-	GitTag    = "v1.4.21"
-	BuildDate = "1676865539"
+	GitCommit = "abef6d05"
+	GitTag    = "v1.5.0"
+	BuildDate = "1676876351"
 )
 
 func Version() string {
